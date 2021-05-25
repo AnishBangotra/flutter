d0e19b09f84e0db0e6280f69c0e64036a1399d2a
Anish Bangotra <73160320+AnishBangotra@users.noreply.github.com>
2021-05-25 21:37:02 +0530

    Add files via upload

---
+++ wordpairGenerator/main.dart
@@ -0,0 +1,16 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import './random_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(primaryColor: Colors.purple[400]),
+        home: RandomWords()
+    );
+  }
+}
